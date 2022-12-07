02fe34d5acefadac0dde6c1965f79a862cec7b38
Ryan <ryan.coutts98@gmail.com>
2022-12-06 20:22:42 -0700

    Change to dictionary
    Changed the list of players to a dictionary to make it easier to keep track of money.
    Also created the "menu" function from the code that was originally in "main".

--- mm.py
+++ mm.py
@@ -1,4 +1,4 @@
-players = []
+players = {}
 
 
 def add_player():
@@ -11,31 +11,15 @@ def add_player():
             break
         elif len(addition) < 3:
             print("Name not added. Each player name must be at least 3 characters.")
+        elif addition.capitalize() in players:
+            print("Player not added. This name is already being used.")
         else:
-            players.append([addition])
-            players[count].append(0)
-    print(f"Player List: {players}")
+            players.update({addition.capitalize(): 0})
+    print(f"Player List: ", end="")
+    for player in players.keys():
+        print(f"{player}, ", end="")
 
-def totals():
-    for i in range(len(players)):
-        print(f"{players[i][0]}: ${players[i][1]}")
-
-def singles_results():
-    while True:
-        w1 = str(input("Which player won? Input b to return to main menu\n>"))
-        l1 = str(input("Which player lost Input b to return to main menu?\n>"))
-
-        if not any(w1 in sublist for sublist in players) or not any (l1 in sublist for sublist in players):
-            print("One of the players you gave has not been added to the "
-                "list. Please try again, or input 'b' to return to the "
-                "main menu to add more players.")
-
-
-
-def main():
-
-    add_player()
-
+def menu():
     while True:
         if len(players) < 2:
             print("\nYou must have 2 or more players to play. Please add at least",
@@ -67,6 +51,41 @@ def main():
                 break
         else:
             print("Invalid Input")
+
+def totals():
+    for player in players:
+        print(f"{player}: ${players[player]}")
+
+def singles_results():
+    while True:
+        winner = str(input("Which player won? Input b to return to main menu"
+            "\n>"))
+        if winner == "b":
+            break
+        loser = str(input("Which player lost Input b to return to main menu?"
+            "\n>"))
+        if loser == "b":
+            break
+        if not any(winner.capitalize() in sublist for sublist in players) or not any (loser.capitalize() in sublist for sublist in players):
+            print("One of the players you gave has not been added to the "
+                "list. Please try again, or input 'b' to return to the "
+                "main menu to add more players.")
+        else:
+            amount = int(input("How much money was this money match for?"
+                "\n>$"))
+            players[winner.capitalize()] += amount
+            players[loser.capitalize()] -= amount
+            break
+    menu()
+
+
+
+def main():
+
+    add_player()
+    menu()
+
+    
 
 main()
 
